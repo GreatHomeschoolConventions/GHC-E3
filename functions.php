17f104aa90fce40ca07cb3cfd2a73fa80d1b771d
Andrew Minion <andrew@andrewrminion.com>
2017-07-24 10:55:23 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Use minified stylesheet
+ */
+function ghc_e3_minified_css() {
+    wp_enqueue_style( 'twentyseventeen-style', get_stylesheet_directory_uri() . '/style.min.css' );
+}
+add_action( 'wp_enqueue_scripts', 'ghc_e3_minified_css', 5 );
+
+/**
+ * Change logo and image sizes
+ */
+function ghc_e3_logo_size() {
+    add_theme_support( 'custom-logo', array(
+        'width'       => 1200,
+        'height'      => 414,
+        'flex-width'  => true,
+    ) );
+
+    add_image_size( 'speaker_xs', 150, 150, true );
+    add_image_size( 'speaker_md', 450, 450, true );
+    add_image_size( 'speaker_lg', 600, 600, true );
+    add_image_size( 'speaker_xl', 900, 900, true );
+    add_image_size( 'twentyseventeen-featured-image', 300, 300, true );
+}
+add_action( 'after_setup_theme', 'ghc_e3_logo_size', 15 );
---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        styles: {
+            files: "SCSS/*.scss",
+            tasks: ['sass', 'postcss'],
+        },
+    },
+    sass: {
+        dev: {
+            files: {
+                "style.min.css" : "SCSS/style.scss",
+            }
+        }
+    },
+    postcss: {
+        options: {
+            map: {
+                inline: false,
+            },
+
+            processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                require('cssnano')() // minify the result
+            ]
+        },
+        dist: {
+            src: '*.min.css',
+        }
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : ['*.css', '**/*.php', '**/*.js', '!node_modules'],
+            },
+            options: {
+                watchTask: true,
+                proxy: "https://ghc.dev",
+                https: {
+                    key: "/Users/andrew/github/dotfiles/local-dev.key",
+                    cert: "/Users/andrew/github/dotfiles/local-dev.crt",
+                }
+            },
+        },
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', [
+        'browserSync',
+        'watch',
+    ]);
+};
