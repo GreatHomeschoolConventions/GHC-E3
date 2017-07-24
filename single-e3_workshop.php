a53b770667706f0430fd02639209fb87e9e10778
Andrew Minion <andrew@andrewrminion.com>
2017-07-24 15:24:26 -0400

    move content template

--- single-e3_workshop.php
+++ single-e3_workshop.php
@@ -20,7 +20,7 @@ get_header(); ?>
             /* Start the Loop */
             while ( have_posts() ) : the_post();
 
-                get_template_part( 'content', get_post_type() );
+                get_template_part( 'template-parts/content/content', get_post_type() );
 
                 // If comments are open or we have at least one comment, load up the comment template.
                 if ( comments_open() || get_comments_number() ) :
