87e571b76e975a0bdb807bce4cb3090720cc1751
Andrew Minion <andrew@andrewrminion.com>
2017-07-24 15:17:18 -0400

    add customized site-info copyright line

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+    &copy;<?php echo date( 'Y' ); ?> <a href="https://www.greathomeschoolconventions.com/">Great Homeschool Conventions</a> | Proudly powered by <a href="https://wordpress.org/" target="_blank">WordPress</a> | Designed and Developed by <a href="https://andrewrminion.com/" target="_blank">AndrewRMinion Design</a>
+</div><!-- .site-info -->
