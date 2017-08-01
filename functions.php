4e41f2b4821029db103b201f470cb63e7cd24462
Andrew Minion <andrew@andrewrminion.com>
2017-07-31 20:24:23 -0400

    tweak checkout styles

--- functions.php
+++ functions.php
@@ -40,3 +40,42 @@ function ghc_e3_remove_order_notes( $fields ) {
 }
 add_filter( 'woocommerce_checkout_fields' , 'ghc_e3_remove_order_notes' );
 add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+
+/**
+ * Add placeholder attributes to checkout fields
+ * @param  array $fields WooCommerce billing fields
+ * @return array modified WooCommerce billing fields
+ */
+function ghc_e3_checkout_billing_placeholders( $fields ) {
+    $fields['billing_first_name']['placeholder'] = 'John';
+    $fields['billing_last_name']['placeholder'] = 'Doe';
+    $fields['billing_address_1']['placeholder'] = '123 Anystreet';
+    $fields['billing_address_2']['placeholder'] = 'Suite 100';
+    $fields['billing_city']['placeholder'] = 'Sometown';
+    $fields['billing_state']['placeholder'] = 'Some State';
+    $fields['billing_postcode']['placeholder'] = '12345';
+    $fields['billing_phone']['placeholder'] = '1-234-567-8901';
+    $fields['billing_email']['placeholder'] = 'john.doe@example.com';
+
+    unset( $fields['billing_company'] );
+
+    return $fields;
+}
+add_filter( 'woocommerce_billing_fields', 'ghc_e3_checkout_billing_placeholders' );
+
+/**
+ * Make checkout password field a text input instead of password
+ * @param  array $fields WooCommerce checkout fields
+ * @return array modified WooCommerce checkout fields
+ */
+function ghc_e3_checkout_password_placeholder( $fields ) {
+    $fields['account']['account_password']['type'] = 'text';
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'ghc_e3_checkout_password_placeholder' );
+
+/**
+ * Check terms and condition by default
+ */
+add_filter( 'woocommerce_terms_is_checked_default', '__return_true' );
