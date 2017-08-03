8416481d31e7a6337a03fb0ba573af3de1285b59
Andrew Minion <andrew@andrewrminion.com>
2017-08-03 14:24:02 -0400

    add GA tracking for purchase buttons

--- js/ghc-e3-workshops.js
+++ js/ghc-e3-workshops.js
@@ -1,9 +1,20 @@
 (function($) {
     $(document).ready(function(){
+        /**
+         * Hidden/expanding content
+         */
         $('.click-to-expand').hide();
         $('.expand-trigger').on('click', function(e) {
             e.preventDefault();
             $(this).toggleClass('expanded').parents().next('.click-to-expand').slideToggle();
+        });
+
+        /**
+         * Purchase page analytics
+         */
+        $('.buy-now .button').on('click', function(e) {
+            var buttonText = $(this).text();
+            ga('send', 'event', 'Button', 'purchase', buttonText);
         });
     });
 })(jQuery);
